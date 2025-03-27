1e8f157a4a2f09b1e875f89877ece7d9201aaff8
Yuvraj Biswal <yuvrajbiswalofficial@gamil.com>
2025-03-27 20:51:31 +0530

    few fixes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-
-const API_URL = "http://localhost:5000/api";
+const dotenv = require("dotenv");
+dotenv.config();
+const API_URL = `${process.env.REACT_APP_API_URL}`;
 
 export const registerUser = async (formData) => {
   return await axios.post(`${API_URL}/auth/register`, formData, {
