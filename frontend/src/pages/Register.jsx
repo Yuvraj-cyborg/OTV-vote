3c9c3118dc2fca249d323d151a2a7b16185729ad
Yuvraj-cyborg <yuvrajbiswalofficial@gmail.com>
2025-04-19 12:14:24 +0530

    atend

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Register() {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+    <h1 className='text-3xl font-bold underline text-center'>Register</h1>
+    </div>
+  )
+}
+
